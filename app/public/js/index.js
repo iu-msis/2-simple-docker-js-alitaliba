e0da0e6bc1e5cef1c15a857ff58bce6e2cf43c2f
aatalib <aatalib@iu.edu>
2021-09-13 11:39:59 -0400

    Create index.js

---
+++ app/public/js/index.js
@@ -0,0 +1,25 @@
+const Offer = {
+    {
+        "offers": [
+            {
+                "id": 1,
+                "name": "Janet Doe",
+                "salary": 70000,
+                "bouns": 1000,
+                "company": "GM",
+                "offer date": "2021-09-08"
+            },
+            {
+                "id": 2,
+                "name": "Chris Ivn",
+                "salary": 85000,
+                "bouns": 3000,
+                "company": "PWC",
+                "offer date": "2021-10-23"
+            }
+        ]
+        
+    }
+  }
+  
+  Vue.createApp(Offer).mount('#offerApp')
