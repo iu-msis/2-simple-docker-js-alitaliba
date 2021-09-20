7f3dbe127c6f4ba47eae46e5252553bc6b07949f
aatalib <aatalib@iu.edu>
2021-09-20 09:52:03 -0400

    Class Notes
    Took notes and changed the index file based on cohort green

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,25 +1,47 @@
 const Offer = {
-    {
+    data() {
+      return {
+        "person": {},
         "offers": [
-            {
-                "id": 1,
-                "name": "Janet Doe",
-                "salary": 70000,
-                "bouns": 1000,
-                "company": "GM",
-                "offer date": "2021-09-08"
-            },
-            {
-                "id": 2,
-                "name": "Chris Ivn",
-                "salary": 85000,
-                "bouns": 3000,
-                "company": "PWC",
-                "offer date": "2021-10-23"
-            }
-        ]
-        
-    }
-  }
+                {
+                    "id": 1,
+                    "name": "Jane Student",
+                    "offer": 100000,
+                    "bonus": 9000,
+                    "company": "EY",
+                    "offerDate": "2021-10-05"
+                },
+                {
+                    "id": 2,
+                    "name": "Jordan Student",
+                    "offer": 87000,
+                    "bonus": 3000,
+                    "company": "IU",
+                    "offerDate": "2021-09-25"
+                }
+            ]
+        }
+    },
+    
+    created() {
+        console.log("A");
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            console.log("C");
+        })
+        .catch( err => {
+            console.error(err)
+        })
+
+        console.log("B");
+    },
+    created() {
+        this.fetchUserData();
+    } //end created
+  }// end Offer config
   
-  Vue.createApp(Offer).mount('#offerApp')
+Vue.createApp(Offer).mount('#offerApp');
