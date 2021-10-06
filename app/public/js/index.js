4f332889f73a6d73d33716dd35da21b0baa4ab41
aatalib <aatalib@iu.edu>
2021-10-06 15:25:29 -0400

    Added api and database

---
+++ app/public/js/index.js
@@ -0,0 +1,53 @@
+const Offer = {
+    data() {
+      return {
+        "students": [],
+        "selectedStudent": null,
+        "offers": []
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        selectStudents(s) {
+            if (this.selectedStudent == s) {
+                return;
+            }
+            this.selectedStudent = s;
+            this.offers = [],
+            this.fetchOfferData(s);
+        },
+        fetchStudentData() {
+            fetch('/api/student/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchOfferData(s) {
+            fetch('/api/student/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+    created() {
+        this.fetchStudentData();
+    } //end created
+} // end Offer config
+  
+Vue.createApp(Offer).mount('#offerApp');
+console.log("Z");
